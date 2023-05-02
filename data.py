50ea095033520b78c8d6ee052c2c3271d8397614
raisinhvh <65270020+raisinhvh@users.noreply.github.com>
2023-05-01 19:58:51 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
-
+enemies = [
+    "NAV" : [ 
+        "Abilities" : ["Bad intentions", "Emergency Tsunami", "Good Habits"],
+        "Drops" : [
+            ["Damage Potion", 0.25],
+            ["Health Potion", 0.25],
+            ["Damage Resistance Potion", 0.25]
+        ],
+        "Health" : 150
+    ]
+]
